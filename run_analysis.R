f006922a7069f7173d7e21b4832183560aa62a58
MitchellJdeJong <30639988+MitchellJdeJong@users.noreply.github.com>
2018-09-24 20:40:51 -0700

    Update run_analysis.R
    Capitalization.

--- run_analysis.R
+++ run_analysis.R
@@ -1,20 +1,21 @@
-# use setwd() to set working directory to the working directory containing the 'weardata' folder
-#load packages
+#Use setwd() to set working directory to the working directory containing the 'weardata' folder
+#setwd()
+
+#Load packages
 library(tidyr)
 library(plyr)
 library(dplyr)
 
-
-#load vector feature names
+#Load vector feature names
 features <- read.table('weardata/UCI HAR Dataset/features.txt')
 
-#grab second column as a vector
+#Gab second column as a vector
 feature.names <- features[['V2']]
 
 #Fix typo where 'Body' appears twice in the feature name
 feature.names <- gsub('BodyBody', 'Body', feature.names)
 
-#load tables
+#Load tables
 training.data <- read.table('weardata/UCI HAR Dataset/train/X_train.txt', col.names = feature.names)
 training.labels <- read.table('weardata/UCI HAR Dataset/train/y_train.txt', col.names = 'exercise')
 training.subjects <- read.table('weardata/UCI HAR Dataset/train/subject_train.txt', col.names = 'subject')
@@ -22,14 +23,14 @@ test.data <- read.table('weardata/UCI HAR Dataset/test/X_test.txt', col.names = 
 test.labels <- read.table('weardata/UCI HAR Dataset/test/y_test.txt', col.names = 'exercise')
 test.subjects <- read.table('weardata/UCI HAR Dataset/test/subject_test.txt', col.names = 'subject')
 
-#replace the 1:6 in the test and training labels with descriptive exercise names
-#it wants to output a character string, so we coerce it back into a data frame. Then we fix the column name.
+#Replace the 1:6 in the test and training labels with descriptive exercise names
+#It wants to output a character string, so we coerce it back into a data frame. Then we fix the column name.
 test.labels <- as.data.frame(mapvalues(test.labels[,1], c('1','2','3','4','5','6'), c('WALKING', 'WALKING_UPSTAIRS', 'WALKING_DOWNSTAIRS', 'SITTING', "STANDING", "LAYING")))
 training.labels <- as.data.frame(mapvalues(training.labels[,1], c('1','2','3','4','5','6'), c('WALKING', 'WALKING_UPSTAIRS', 'WALKING_DOWNSTAIRS', 'SITTING', "STANDING", "LAYING")))
 names(test.labels) <- 'exercise'
 names(training.labels) <- 'exercise'
 
-#add the subject number and the labels to the training and test data
+#Add the subject number and the labels to the training and test data
 combined.training.data <- cbind(training.subjects, training.labels, training.data)
 combined.test.data <- cbind(test.subjects, test.labels, test.data)
 #Combine it all into one data frame.
@@ -54,10 +55,10 @@ sdev.mean.df <- combined.df[,c(1:2, sdev.and.mean.cols + 2)]
 
 sdev.mean.df <- gather(sdev.mean.df, measurement, value, -subject, -exercise)
 
-#group by subject, exercise, and measurement
+#Group by subject, exercise, and measurement
 sdev.mean.df <- group_by(sdev.mean.df, subject, exercise, measurement)
 
-#summarize data to get the mean of each group
+#Summarize data to get the mean of each group
 
 tidy.df <- dplyr::summarize(sdev.mean.df, mean_value = mean(value))
 
